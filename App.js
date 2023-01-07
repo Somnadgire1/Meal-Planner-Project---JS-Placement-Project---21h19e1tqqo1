e39f9079e0033bbc2d229e0aa46666e720edc93b
Somnath_Nadgire <somnathnadgire1@gmail.com>
2023-01-07 19:57:09 +0530

    k

--- App.js
+++ App.js
@@ -1,1 +1,197 @@
+const heightInput = document.getElementById("height");
+const weightInput = document.getElementById("weight");
+const ageInput = document.getElementById("age");
+const genderInput = document.getElementById("gender");
+const activityInput = document.getElementById("activity");
+const submit = document.getElementById("submitBtn");
+const cardContainer = document.getElementById("cards-container");
+const mealsDetails = document.getElementById("details");
+const ingredientSection = document.getElementById("ingredients");
+const stepsSection = document.getElementById("steps");
+const equipmentSection = document.getElementById("equipment");
+const recipeSection = document.getElementById("recipe-section");
+const API_KEY = "08d3f200ea974b08913e1cbdc042dbf4";
 
+// calori calculation
+const getCalorie = () => {
+  let hv = heightInput.value;
+  let wv = weightInput.value;
+  let av = ageInput.value;
+  let gv = genderInput.value;
+  let avv = activityInput.value;
+  let bmr;
+
+  if (hv === "" || hv <= 0 || wv === "" || wv <= 0 || av === "" || av <= 0) {
+    alert("Input field must Required");
+    return;
+  }
+
+  if (gv === "female") {
+    //**For women**
+    bmr = 655.1 + 9.563 * wv + 1.85 * hv - 4.676 * av;
+  } else if (gv === "male") {
+    //**For men**
+    bmr = 66.47 + 13.75 * wv + 5.003 * hv - 6.755 * av;
+  }
+
+  // Daily Calorie Requirement
+  if (avv === "light") {
+    //**Lightly active (exercise 1–3 days/week)**
+    bmr *= 1.375;
+  } else if (avv === "moderate") {
+    //**Moderately active (exercise 3–5 days/week)**
+    bmr *= 1.55;
+  } else if (avv === "active") {
+    //**Active (exercise 6–7 days/week)**
+    bmr *= 1.725;
+  }
+
+  getMeals(bmr);
+};
+
+const getMeals = async (bmr) => {
+  document.getElementById("loader").style.display = "block";
+  const url = `https://api.spoonacular.com//mealplanner/generate?timeFrame=day&targetCalories=${bmr}&apiKey=${API_KEY}&includeNutrition=true`;
+
+  let datas;
+  await fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      datas = data;
+    });
+  generateMealsCard(datas);
+  document.getElementById("loader").style.display = "none";
+};
+
+const generateMealsCard = (datas) => {
+  let cards = ``;
+  mealsDetails.innerHTML = `
+    <div class="title-meal">
+        <span>BREAKFAST</span>
+        <span>LUNCH</span>
+        <span>DINER</span>
+    </div>
+    `;
+  datas.meals.map(async (data) => {
+    const url = `https://api.spoonacular.com/recipes/${data.id}/information?apiKey=${API_KEY}&includeNutrition=false`;
+    let imgURL;
+    await fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        imgURL = data.image;
+      });
+    cards += `
+        <div class="col-md-4 d-flex justify-content-center mb-2">
+            <div class="card" style="width: 18rem;">
+                <img src=${imgURL} class="card-img-top"
+                    alt="meal 1">
+                <div class="card-body">
+                    <h5 class="card-title">${data.title}</h5>
+                    <p class="px-2">Calories : ${datas?.nutrients?.calories}</p>
+                    <button class="btn-recipe" onClick="btnRecipe(${data.id})" >Get Recipe</button>
+                </div>
+            </div>
+        </div>
+        `;
+    cardContainer.innerHTML = cards;
+  });
+};
+//fetch recipy
+const btnRecipe = async (data) => {
+  recipeSection.innerHTML = "";
+  ingredientSection.innerHTML = "";
+  stepsSection.innerHTML = "";
+  equipmentSection.innerHTML = "";
+  const url = `https://api.spoonacular.com/recipes/${data}/information?apiKey=${API_KEY}&includeNutrition=false`;
+  let information;
+
+  await fetch(url)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      information = data;
+    });
+  //title of recipy
+  recipeSection.textContent = `${information.title} Recipe`;
+
+  //   Ingridents
+  let htmlData = ``;
+  let inCardDiv = document.createElement("div");
+  inCardDiv.classList.add("carddesign", "card", "h-100");
+  let inCardBody = document.createElement("div");
+  inCardBody.classList.add("card-body");
+  let inOverlay = document.createElement("div");
+  inOverlay.classList.add("overlay");
+  let ol = document.createElement("ol");
+  information.extendedIngredients.map((ingre) => {
+    htmlData += `<li>${ingre.original}</li>`;
+  });
+  ol.innerHTML = htmlData;
+  let ingreH1 = document.createElement("h3");
+  ingreH1.textContent = "INGREDIENTS";
+  inCardBody.appendChild(inOverlay);
+  inCardBody.appendChild(ingreH1);
+  inCardBody.appendChild(ol);
+  inCardDiv.appendChild(inCardBody);
+  ingredientSection.appendChild(inCardDiv);
+
+  //   Steps
+  let stepsHtml = ``;
+  let stCardDiv = document.createElement("div");
+  stCardDiv.classList.add("carddesign", "card", "h-100");
+  let stCardBody = document.createElement("div");
+  stCardBody.classList.add("card-body");
+  let stOverlay = document.createElement("div");
+  stOverlay.classList.add("overlay");
+  let stepsOl = document.createElement("ol");
+  information.analyzedInstructions[0].steps.map((step) => {
+    stepsHtml += `<li>${step.step}</li>`;
+  });
+  stepsOl.innerHTML = stepsHtml;
+  let stepsH1 = document.createElement("h3");
+  stepsH1.textContent = "STEPS";
+  stCardBody.appendChild(stOverlay);
+  stCardBody.appendChild(stepsH1);
+  stCardBody.appendChild(stepsOl);
+  stCardDiv.appendChild(stCardBody);
+  stepsSection.appendChild(stCardDiv);
+
+  // fetch equipments
+  const urlEquip = `https://api.spoonacular.com/recipes/${data}/equipmentWidget.json?apiKey=${API_KEY}&includeNutrition=false`;
+  let equip;
+
+  await fetch(urlEquip)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      equip = data;
+    });
+
+  let equipData = ``;
+  let eqCardDiv = document.createElement("div");
+  eqCardDiv.classList.add("carddesign", "card", "h-100");
+  let eqCardBody = document.createElement("div");
+  eqCardBody.classList.add("card-body");
+  let eqOverlay = document.createElement("div");
+  eqOverlay.classList.add("overlay");
+  let equipUl = document.createElement("ol");
+  equip.equipment.map((equip) => {
+    equipData += `<li>${equip.name}</li>`;
+  });
+  equipUl.innerHTML = equipData;
+  let equipH1 = document.createElement("h3");
+  equipH1.textContent = "EQUIPMENT";
+  eqCardBody.appendChild(eqOverlay);
+  eqCardBody.appendChild(equipH1);
+  eqCardBody.appendChild(equipUl);
+  eqCardDiv.appendChild(eqCardBody);
+  equipmentSection.appendChild(eqCardDiv);
+};
+
+submit.addEventListener("click", getCalorie);
--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 # html-css-js-project-boilerplate
+# Meal Planner Project
+#API_KEY 1- 08d3f200ea974b08913e1cbdc042dbf4
+#API_KEY 2- 3a147c8ef9854d828c785764252802a0
+## Project o**bjective**
+
+- You will be building an meal planner application using html, css and javascript
+- In the end, we’ll have a web application which takes user data and generates meal plan for them along with their recipes.
+
+![Untitled](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/ff0ee167-421b-4b48-b2d9-cfe3a45cdc63/Untitled.png)
+
+## **Project Context**
+
+- It has become difficult to manage and plan daily meals for the average citizen due to the modern busy lifestyle. Among the enormous data present in the internet it becomes very time consuming to find new recipes and get all details of that recipe in one place.
+- We will explore a way to meet the user's meal planning needs using the Spoonacular API. ([https://spoonacular.com/food-api](https://spoonacular.com/food-api))
+
+**Tech stack Prerequisite:**  HTML, CSS, JavaScript
+
+## ****Project Steps****
+
+- **Step-1**: Create Api Key
+    - Create an account in [https://spoonacular.com/food-api](https://spoonacular.com/food-api) and get the api key which you would use in the project.
+- **Step-2**: Nav bar
+    - The website should have a `navigation` section.
+    - The `nav` bar should have the logo of your website
+- **Step-3**: User Form
+    - This `user-form` will be used to take user data and calculate the daily calorie requirement of the user.
+    - The form should have five input fields, `weight`(kg), `height`(cm), `age`, `gender` and `physical activity` level. The `physical activity` should have three options, `light, moderate and active.`
+    - The form should have a `submit` button
+- **Step-4**: Meal Plan
+    - The meal-plan section would have 3 meal cards, for breakfast, lunch and dinner respectively.
+    - Each of the meal cards should have the following: `meal image`, `meal name`, `calories` of the meal and a `get-recipe` button.
+- **Step-5**: Recipe
+    - The recipe section should have 3 tabs - `Ingredients`, `steps` and `Equipment`.
+    - The ingredients tab would display the list of ingredients along with their quantity required for the recipe.
+    - The equipment tab displays a list of equipment needed for the recipe.
+    - The steps tab would display a ordered list of steps that need to be followed.
+- **Step-6**: Daily Calorie Requirement
+    - On clicking the user form submit button, take all the inputs from the user form and calculate the BMR of the user. The formula for BMR is
+        
+        **For women**, BMR = 655.1 + (9.563 x weight in kg) + (1.850 x height in cm) - (4.676 x age in years)
+        
+        **For men**, BMR = 66.47 + (13.75 x weight in kg) + (5.003 x height in cm) - (6.755 x age in years)
+        
+    - Calculate the daily calorie requirement from the BMR. The formula varies for different levels of activity level.
+        - **Lightly active (exercise 1–3 days/week)**: BMR x 1.375
+        - **Moderately active (exercise 3–5 days/week)**: BMR x 1.55
+        - **Active (exercise 6–7 days/week)**: BMR x 1.725
+- **Step-7**: Generate Meal Plan
+    - Once the daily calorie requirement is calculated, use the spoonacular api ([https://spoonacular.com/food-api/docs#Generate-Meal-Plan](https://spoonacular.com/food-api/docs#Generate-Meal-Plan)) to **generate a** **new meal plan**. The api would return three meals each for **breakfast, lunch and dinne**r.
+    - Populate the `meal-plan` section with the data received from the api.
+    - The `meal-plan` section should be displayed only after the meal plan is generated from the daily calorie requirement.
+- **Step 8**: Generate Recipe
+    - When the user clicks on the `get-recipe` button on one of the meals in the meal plan, use the `id` of that meal to fetch its recipe from the api ([https://spoonacular.com/food-api/docs#Get-Recipe-Information](https://spoonacular.com/food-api/docs#Get-Recipe-Information) )
+    - Use the **data** received from the api to populate all the three tabs of the recipe section.
+    - The `recipe-section` should only be displayed once the data is fetched from the api.
+
+## Project Checkpoints
+
+- The navbar and the user form
+- The meal plan and the recipe section
+- Daily Calorie Calculation and Meal Plan Generation.
+- Fetching and displaying of recipes.
